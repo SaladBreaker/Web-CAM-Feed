b35ff5adea24f774e99ae7830a43440141d5279e
vlad.luca <vlad.luca@syneto.eu>
2021-08-22 12:35:28 +0300

    Added flask app

---
+++ src/webcam-feed-flask/camera.py
@@ -0,0 +1,23 @@
+import cv2
+
+camera = cv2.VideoCapture(3)
+# 0 - integrated cam
+# 3 - usb camera
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
+for local webcam use cv2.VideoCapture(0)
+'''
+
+
+# set the format into MJPG in the FourCC format
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
